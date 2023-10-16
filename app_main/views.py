c6dc6978fffc72ba28a990adae02eac5b5d1e3b7
Sanaullah Rabbi <srrabbi01@gmail.com>
2023-10-16 16:24:00 +0600

    change app_main template path

--- app_main/views.py
+++ app_main/views.py
@@ -6,34 +6,36 @@ from .models import *
 
 # Create your views here.
 
+
 def index_view(request):
-    notice = Notice.objects.all().order_by('-date')[:4]
-    context = {
-        'notice': notice
-    }
-    return render(request,'common/index.html', context)
+    notice = Notice.objects.all().order_by("-date")[:4]
+    context = {"notice": notice}
+    return render(request, "app_main/index.html", context)
+
 
 def about_us_view(request):
-    return render(request,'common/about_us.html')
+    return render(request, "app_main/about_us.html")
+
 
 def course_view(request):
-    return render(request,'common/course.html')
+    return render(request, "app_main/course.html")
+
 
 def admission_view(request):
-    return render(request,'common/admission.html')
+    return render(request, "app_main/admission.html")
+
 
 def teachers_view(request):
     teachers_obj = Teacher.objects.all()
-    context = {
-        'teachers_obj': teachers_obj
-    }
-    return render(request,'common/teachers.html', context)
+    context = {"teachers_obj": teachers_obj}
+    return render(request, "app_main/teachers.html", context)
+
 
 def notice_view(request):
-    notice_obj = Notice.objects.all().order_by('-date')
+    notice_obj = Notice.objects.all().order_by("-date")
     all_post = Paginator(notice_obj, 6)
-    page = request.GET.get('page')
-    notice_imagesObj = NoticeImages.objects.all().order_by('-id')
+    page = request.GET.get("page")
+    notice_imagesObj = NoticeImages.objects.all().order_by("-id")
     try:
         notice = all_post.page(page)
     except PageNotAnInteger:
@@ -42,26 +44,23 @@ def notice_view(request):
         notice = all_post.page(all_post.num_pages)
 
     context = {
-        'notice': notice,
-        'notice_imagesObj':notice_imagesObj,
+        "notice": notice,
+        "notice_imagesObj": notice_imagesObj,
     }
-    return render(request,'common/notice.html', context)
+    return render(request, "app_main/notice.html", context)
+
 
 def contact_us_view(request):
     form = ContactForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your message has been sent successfully!')
-            return redirect('contactus')
+            messages.success(request, "Your message has been sent successfully!")
+            return redirect("contactus")
 
-
-    context = {
-        'form' : form
-    }
-    return render(request,'common/contact_us.html', context)
-
+    context = {"form": form}
+    return render(request, "app_main/contact_us.html", context)
 
 
 def custom_page_not_found_view(request, exception=None):
